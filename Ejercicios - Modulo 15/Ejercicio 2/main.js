ff91874e10a023b894f184808074b9e7f37d417d
ChristianLeiva <chriistiianleiiva@gmail.com>
2022-07-28 00:34:06 -0300

    se agregaron los ejercicios del modulo 15

---
+++ Ejercicios - Modulo 15/Ejercicio 2/main.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelector("#buttonSubmit").addEventListener("click", (e)=>{
+    e.preventDefault()
+    const money = document.querySelector("#dinero").value;
+    const necessaryExpenses = document.querySelector("#gastosNecesarios").value;
+    const optionalExpenses = document.querySelector("#gastosOpcionales").value;
+    const saving = document.querySelector("#ahorro").value;
+
+    const result = parseFloat( necessaryExpenses ) +parseFloat( optionalExpenses ) + parseFloat( saving )
+    
+    if(result > 1){
+        document.querySelector("#error").innerHTML = "<p>ERROR!! Los porcentajes superan el valor del dinero ingresado</p>"
+    }else{
+        document.querySelector("#resultGastosNecesarios").value = (money * necessaryExpenses)
+        document.querySelector("#resultGastosOpcionales").value = (money * optionalExpenses)
+        document.querySelector("#resultAhorro").value = (money * saving)
+
+    }
+
+
+})
